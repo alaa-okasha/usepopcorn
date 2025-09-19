dfac53742840c3173b897de42a50635c64e9164b
Alaa <aayyaser@hotmail.com>
2025-09-20 02:19:46 +0300

    fixing api bug

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ export default function App() {
         setError("");
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
           { signal: controller.signal }
         );
         if (!res.ok) {
